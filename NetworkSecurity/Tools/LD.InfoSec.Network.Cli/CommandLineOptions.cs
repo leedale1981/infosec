9973679e66e4271ea20ea6c8ee68f8e5c4891a5a
Lee Dale <leedale@proton.me>
2024-02-17 15:20:54 +0000

    Adding command line parser and discovery project

---
+++ NetworkSecurity/Tools/LD.InfoSec.Network.Cli/CommandLineOptions.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+
+namespace LD.InfoSec.Network.Cli;
+
+public class CommandLineOptions
+{
+    [Value(index: 0, Required = true, HelpText="Network action to execute. DoS, Discover, Scan")]
+    public string Action { get; set; }
+
+    [Option(shortName: 't', longName: "target", Required = true, HelpText = "IP address of target")]
+    public string TargetIp { get; set; }
+    
+    [Option(shortName: 'p', longName: "port", Required = false, HelpText = "Port on target")]
+    public string TargetPort { get; set; }
+    
+    [Option(shortName: 's', longName: "source", Required = false, HelpText = "IP address of source")]
+    public string SourceIp { get; set; }
+}
---
+++ NetworkSecurity/Tools/LD.InfoSec.Network.Discovery/DiscoveryService.cs
@@ -0,0 +1,6 @@
+﻿namespace LD.InfoSec.Network.Discovery;
+
+public class DiscoveryService
+{
+
+}
