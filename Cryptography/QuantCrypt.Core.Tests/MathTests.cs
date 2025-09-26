69c0708119867972819e1b18ef5d6d50899d242b
Lee Dale <leedale@proton.me>
2025-09-26 16:12:45 +0100

    Adding polynomial mod operations for PQC KEM

---
+++ Cryptography/QuantCrypt.Core.Tests/MathTests.cs
@@ -0,0 +1,42 @@
+﻿using QuantCrypt.Core.Math;
+
+namespace QuantCrypt.Core.Tests;
+
+public class MathTests
+{
+    [Fact]
+    public void ModPolynomial_Add_Successful_Test()
+    {
+        // Arrange
+        Polynomial[] polynomialA = new[]
+        {
+            new Polynomial(2, 4),
+            new Polynomial(3, 3),
+            new Polynomial(10, 1),
+            new Polynomial(3, 0)
+        };
+
+        Polynomial[] polynomialB = new[]
+        {
+            new Polynomial(3, 5),
+            new Polynomial(14, 3),
+            new Polynomial(10, 1),
+            new Polynomial(4, 0)
+        };
+
+        // Act
+        Polynomial[] actualResult = PolynomialOperations.AddMod(polynomialA, polynomialB, 17);
+        
+        // Assert
+        Polynomial[] expectedResult = new[]
+        {
+            new Polynomial(3, 5),
+            new Polynomial(2, 4),
+            new Polynomial(3, 1),
+            new Polynomial(7, 0)
+        };
+        expectedResult.OrderByDescending(p => p.Order);
+
+        Assert.True(expectedResult.SequenceEqual(actualResult));
+    }
+}
---
+++ Cryptography/QuantCrypt.Core/Math/PolynomialOperations.cs
@@ -0,0 +1,27 @@
+﻿namespace QuantCrypt.Core.Math;
+
+public record Polynomial(int Coefficient, int Order);
+
+public static class PolynomialOperations
+{
+    public static Polynomial[] AddMod(Polynomial[] polynomialA, Polynomial[] polynomialB, int mod)
+    {
+        List<Polynomial> result = new();
+
+        foreach (Polynomial poly in polynomialA)
+        {
+            Polynomial? orderMatchPoly = polynomialB.FirstOrDefault(p => p.Order == poly.Order);
+            result.Add(orderMatchPoly is null ? 
+                poly with { Coefficient = poly.Coefficient % mod } : 
+                poly with { Coefficient = (poly.Coefficient + orderMatchPoly.Coefficient) % mod});
+        }
+
+        result.AddRange(polynomialB
+            .Where(p => result.All(r => r.Order != p.Order))
+            .Select(p => p with { Coefficient = p.Coefficient % mod }));
+        
+        return result
+            .Where(p => p.Coefficient != 0)
+            .OrderByDescending(p => p.Order).ToArray();
+    }
+}
